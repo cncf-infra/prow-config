9887b6c6a5e5684ca9874371a5ac96c2adb677f2
Caleb Woodbine <calebwoodbine.public@gmail.com>
2022-02-22 13:47:26 +1300

    Update all dependencies

--- prow/external-plugins/verify-conformance-tests/plugin/plugin.go
+++ prow/external-plugins/verify-conformance-tests/plugin/plugin.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2020 CNCF TODO Check how this code should be licensed
+Copyright 2020-2022 CNCF TODO Check how this code should be licensed
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
