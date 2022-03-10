56599797e76bfab2e569c6a15e074dc2df6c0eb1
Caleb Woodbine <calebwoodbine.public@gmail.com>
2022-03-11 09:10:58 +1300

    Update eks configuration

--- infra/aws/vpc_variables.tf
+++ infra/aws/vpc_variables.tf
@@ -1,22 +1,24 @@
 ############### VPC variables
 variable "name" {
   type        = string
-  default = "eks-bottlerocket-imnr"
+  default     = "prow-cncf-io-eks"
   description = "A name for this stack."
 }
 
 variable "region" {
   type        = string
-  default     = "us-east-1"
+  default     = "ap-southeast-2"
   description = "Region where this stack will be deployed."
 }
+
 variable "cidr_block" {
   type        = string
   default     = "10.0.0.0/16"
   description = "The CIDR block for the VPC."
 }
+
 variable "availability_zones" {
-  default = ["us-east-1a", "us-east-1b", "us-east-1c"]
+  default     = ["ap-southeast-2a", "ap-southeast-2b", "ap-southeast-2c"]
   description = "The availability zones to create subnets in"
 }
 
