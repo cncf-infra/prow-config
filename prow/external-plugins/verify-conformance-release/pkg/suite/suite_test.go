d68a4ed83dc1e2a1811cef5c182fdf598fd49cdf
Caleb Woodbine <calebwoodbine.public@gmail.com>
2022-07-28 16:51:28 +1200

    Update first test

--- prow/external-plugins/verify-conformance-release/pkg/suite/suite_test.go
+++ prow/external-plugins/verify-conformance-release/pkg/suite/suite_test.go
@@ -68,28 +68,61 @@ func TestNewPRSuite(t *testing.T) {
 type testSuiteCase struct {
 	Name            string
 	PullRequest     *PullRequest
-	PRSuiteOptions  PRSuiteOptions
 	ExpectedComment string
 	ExpectedLabels  []string
 }
 
 func TestNewTestSuite(t *testing.T) {
-	prSuiteOptionsDefault := PRSuiteOptions{
+	prSuiteOptions := PRSuiteOptions{
 		Paths: []string{"../../kodata/features/"},
 	}
 	cases := []testSuiteCase{
 		{
-			PullRequest:     &PullRequest{},
-			PRSuiteOptions:  prSuiteOptionsDefault,
-			ExpectedComment: ``,
-			ExpectedLabels:  []string{"conformance-product-submission", "release-documents-checked"},
+			Name:        "empty pr",
+			PullRequest: &PullRequest{},
+			ExpectedComment: `0 of 14 requirements have passed. Please review the following:
+- [FAIL] it seems that there is no title set
+  - title is empty
+- [FAIL] there seems to be some required files missing (https://github.com/cncf/k8s-conformance/blob/master/instructions.md#contents-of-the-pr)
+  - missing file &#39;README.md&#39;
+  - missing file &#39;PRODUCT.yaml&#39;
+  - missing file &#39;e2e.log&#39;
+  - missing file &#39;junit_01.xml&#39;
+- [FAIL] the submission file directory does not seem to match the Kubernetes release version in the files
+  - there were no files found in the submission
+- [FAIL] the submission seems to contain files of multiple Kubernetes release versions or products. Each Kubernetes release version and products should be submitted in a separate PRs
+  - there were no files found in the submission
+- [FAIL] the title of the submission does not seem to contain a Kubernetes release version that matches the release version in the submitted files
+  - there were no files found in the submission
+- [FAIL] it appears that the PRODUCT.yaml file does not contain all the required fields (https://github.com/cncf/k8s-conformance/blob/master/instructions.md#productyaml)
+  - missing required file &#39;PRODUCT.yaml&#39;
+- [FAIL] it appears that URL(s) in the PRODUCT.yaml aren't correctly formatted URLs
+  - missing required file &#39;PRODUCT.yaml&#39;
+- [FAIL] it appears that URL(s) in the PRODUCT.yaml don't resolve to the correct data type
+  - missing required file &#39;PRODUCT.yaml&#39;
+- [FAIL] the submission title is missing either a Kubernetes release version (v1.xx) or product name
+  - title is empty
+- [FAIL] it seems the e2e.log does not contain the Kubernetes release version that match the submission title
+  - missing required file &#39;e2e.log&#39;
+- [FAIL] the Kubernetes release version in this pull request does not qualify for conformance submission anymore (https://github.com/cncf/k8s-conformance/blob/master/terms-conditions/Certified_Kubernetes_Terms.md#qualifying-offerings-and-self-testing)
+  - unable to find a Kubernetes release version in the title
+- [FAIL] it appears that some tests are missing from the product submission
+  - missing required file &#39;junit_01.xml&#39;
+- [FAIL] it appears that some tests failed in the product submission
+  - missing required file &#39;e2e.log&#39;
+- [FAIL] it appears that there is a mismatch of tests in junit_01.xml and e2e.log
+  - missing required file &#39;e2e.log&#39;
+
+ for a full list of requirements, please refer to these sections of the docs: [_content of the PR_](https://github.com/cncf/k8s-conformance/blob/master/instructions.md#contents-of-the-pr), and [_requirements_](https://github.com/cncf/k8s-conformance/blob/master/instructions.md#requirements).
+`,
+			ExpectedLabels: []string{"conformance-product-submission", "release-documents-checked", "missing-file-README.md", "missing-file-PRODUCT.yaml", "missing-file-e2e.log", "missing-file-junit_01.xml", "not-verifiable"},
 		},
 	}
 	for _, c := range cases {
 		t.Run(c.Name, func(t *testing.T) {
 			t.Parallel()
 			s := NewPRSuite(c.PullRequest)
-			s.NewTestSuite(c.PRSuiteOptions).Run()
+			s.NewTestSuite(prSuiteOptions).Run()
 			comment, labels, _ := s.GetLabelsAndCommentsFromSuiteResultsBuffer()
 			if comment != c.ExpectedComment {
 				fmt.Println(comment)
